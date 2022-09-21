24f25896158b8724ba41e93a5aa808064e78cb06
Priyam Ghosh <devops@Priyams-MacBook-Air.local>
2022-09-22 01:43:49 +0530

    changing index.js--4

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const app = express();
 
-app.get('/', async (request, response) => {
+app.get('/', (request, response) => {
 
     console.log("Hello World");
-    return response.status(200).send("Hello Priyam");
+    //return response.status(200).send("Hello Priyam");
 
-    let transporter = nodemailer.createTransport({
-        name: 'godspeedgames.com',
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: "20983252ca9ddc", // generated ethereal user
-            pass: "261a77a8b36029" // generated ethereal password
-        },
-    });
+    // let transporter = nodemailer.createTransport({
+    //     name: 'godspeedgames.com',
+    //     host: "smtp.mailtrap.io",
+    //     port: 2525,
+    //     secure: false, // true for 465, false for other ports
+    //     auth: {
+    //         user: "20983252ca9ddc", // generated ethereal user
+    //         pass: "261a77a8b36029" // generated ethereal password
+    //     },
+    // });
 
-    let message = {
-        from: 'admin@godspeedgames.com', // sender address
-        to: 'priyam.g.codopoliz@gmail.com', // list of receivers
-        subject: "Email Verification - Godspeedgames Webshop", // Subject line
-        text: 'OTP : 1234' // plain text body
-    };
+    // let message = {
+    //     from: 'admin@godspeedgames.com', // sender address
+    //     to: 'priyam.g.codopoliz@gmail.com', // list of receivers
+    //     subject: "Email Verification - Godspeedgames Webshop", // Subject line
+    //     text: 'OTP : 1234' // plain text body
+    // };
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail(message, (error, info) => {
-        if (error) {
-            console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        response.status(200).send(info.messageId);
-    });
+    // // send mail with defined transport object
+    // let info = await transporter.sendMail(message, (error, info) => {
+    //     if (error) {
+    //         console.log(error);
+    //     }
+    //     console.log('Message sent: %s', info.messageId);
+    //     response.status(200).send(info.messageId);
+    // });
 
     
 });
