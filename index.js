545945cfe4db89baa90ceaf7d64d56be09cd1c5a
Priyam Ghosh <devops@Priyams-MacBook-Air.local>
2022-09-23 20:52:27 +0530

    Changing index.js ... 25

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.get('/', async (req, res) => {
         console.log("Credentials obtained, sending message...");
 
         let transporter = nodemailer.createTransport({
-            name: '157.230.191.79:5000',
-            host: "smtp.ethereal.email",
+            name: '157.230.191.79',
+            host: "157.230.191.79",
+            service: "smtp.ethereal.email",
             port: 587,
             secure: false, // true for 465, false for other ports
             auth: {
